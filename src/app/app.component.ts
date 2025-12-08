77fe900b64691a332567b7f9537eeb8ca5eb8872
gokul <gokulmittu01@gmail.com>
2025-12-08 18:03:40 +0530

    'updated_files'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component'; 
 import { FooterComponent } from './footer/footer.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent,BrowserAnimationsModule],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   template: `
     <app-navbar></app-navbar>
     <router-outlet></router-outlet>
