11d393c29f480dc0216e9a8bd2697fcf655190e2
gokul <gokulmittu01@gmail.com>
2026-05-07 23:28:30 +0530

    updated home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,12 @@
-// app.component.ts
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './navbar/navbar.component';
-import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
-  template: `
-  <app-navbar></app-navbar>
-  
-  <main class="main-content page-content-wrapper page-transition-wrapper">
-    <router-outlet></router-outlet>
-  </main>
-
-  <app-footer></app-footer>
-`,
-  styles: [`
-    .main-content {
-      min-height: 100vh;
-      width: 100%;
-      max-width: 100vw;
-      overflow-x: hidden;
-      background: var(--bg-light);
-      position: relative;
-    }
-  `]
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Tail Cart Frontend';
+  title = 'devu_vignesh_wedding';
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,8 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withInMemoryScrolling } from '@angular/router';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(routes, withInMemoryScrolling({ scrollPositionRestoration: 'top' })),
-    provideHttpClient(),
-    provideAnimationsAsync()
-  ]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
 };
